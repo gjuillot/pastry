1fa55588b2821402c15b28502fc58464d71a14e7
Guillaume JUILLOT <guillaume.juillot@alcatel-lucent.com>
2014-04-03 16:16:43 +0200

    Add categories to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,14 +1,15 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :name, :quantity, :unit
+  attr_accessible :name, :quantity, :unit, :category
   has_many :recipe_steps
   
-  UNITS = ["personnes", "parts"]
+  UNITS = ["personnes", "parts", "g", "cl"]
+  CATEGORIES = ["Base", "Entremet", "Autre"]
   
   def steps
     recipe_steps
   end
   
   def cost
-    steps.map(&:cost).inject(:+)
+    steps.map(&:cost).inject(0, :+)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140330183150) do
+ActiveRecord::Schema.define(:version => 20140403085115) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20140330183150) do
     t.string   "unit"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "category"
   end
 
   create_table "users", :force => true do |t|
