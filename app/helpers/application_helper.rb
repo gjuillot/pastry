9febc7cd25b656da2c51ab3914a01c1879427574
Guillaume JUILLOT <guillaume.juillot@alcatel-lucent.com>
2014-05-07 14:42:58 +0200

    Can delete ingredients

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def delete_icon(object)
+    raw "<a href='#{url_for(object)}' data-method='delete' data-confirm='Etes-vous sur ?' rel='nofollow'><span class='glyphicon glyphicon-trash'></span></a>"
+  end
 end
