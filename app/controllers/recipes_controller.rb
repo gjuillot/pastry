13108e150d984a9ceec65b064f920c4da4e7ce01
Guillaume JUILLOT <guillaume.juillot@alcatel-lucent.com>
2014-05-18 10:51:04 +0200

    Consultation view instead of modification view as default view for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,7 +10,7 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
-    @edit_mode = params[:edit_mode].blank? ? true : (params[:edit_mode] == "true")
+    @edit_mode = params[:edit_mode].blank? ? false : (params[:edit_mode] == "true")
     
     if (params[:quantity].blank?)
       @quantity = @recipe.quantity
