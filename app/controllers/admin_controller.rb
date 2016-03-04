cb9d4b2703209d9473dbf3cdca6f1c45d74f1a31
Guillaume Juillot <guillaume.juillot@gmail.com>
2016-03-04 19:02:59 +0000

    Restrict access for some page to only 1st user

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,9 +8,24 @@ class AdminController < ApplicationController
   end
   
   def macarons
+    if (current_user.nil? or current_user.id != 1)
+      redirect_to @@forbidden
+      return
+    end
+  end
+  
+  def products
+    if (current_user.nil? or current_user.id != 1)
+      redirect_to @@forbidden
+      return
+    end
   end
   
   def html_snippets
+    if (current_user.nil? or current_user.id != 1)
+      redirect_to @@forbidden
+      return
+    end
   end
 
 end
