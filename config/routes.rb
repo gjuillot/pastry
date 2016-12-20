72b4684ac8d77884b672bdd8b81483c4750c2eec
Guillaume Juillot <guillaume.juillot@gmail.com>
2016-12-20 18:32:25 +0000

    Pause of the activity

--- config/routes.rb
+++ config/routes.rb
@@ -18,21 +18,6 @@ Pastry::Application.routes.draw do
   resources :investments
   resources :investment_categories
   
-  resources :macarons_geants
-  resources :tartes
-  resources :entremets
-  resources :patisseries_americaines
-  
-  match '/index', :to => "home#index"
-  match '/produits', :to => "home#produits"
-  match '/macarons', :to => "home#macarons"
-  match '/desserts_glaces', :to => "home#desserts_glaces"
-  match '/gateaux_decores', :to => "home#gateaux_decores"
-  match '/pour_vos_evenements', :to => "home#pour_vos_evenements"
-  match '/evenements', :to => "home#evenements"
-  match '/partenaires', :to => "home#partenaires"
-  match '/contact', :to => "home#contact"
-  match '/legal', :to => "home#legal"
   match '/admin', :to => "home#admin"
-  root :to => "home#index"
+  root :to => "home#welcome"
 end
