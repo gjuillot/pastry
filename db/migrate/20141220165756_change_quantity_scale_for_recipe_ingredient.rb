f00946af634e6fe21e59c42ede8627c92209fc4c
Guillaume Juillot <guillaume.juillot@gmail.com>
2017-01-31 16:46:04 +0000

    Fix migration file

--- db/migrate/20141220165756_change_quantity_scale_for_recipe_ingredient.rb
+++ db/migrate/20141220165756_change_quantity_scale_for_recipe_ingredient.rb
@@ -1,5 +1,5 @@
 class ChangeQuantityScaleForRecipeIngredient < ActiveRecord::Migration
   def change
-    change_column :recipe_ingredients, :quantity, :decimal, :scale => 2
+    change_column :recipe_ingredients, :quantity, :decimal, :precision => 10, :scale => 2
   end
 end
